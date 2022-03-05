f6417754ba85f880ac22c8a3ffabe753ed16b6e2
sabaakram021 <sabaakram211@gmail.com>
2022-03-05 22:31:18 +0530

    update

--- function.js
+++ function.js
@@ -3,3 +3,11 @@ function sayHello()
     console.log("hello from function");
 }
 sayHello();
+//function with parameter
+
+function sum(num1,num2)
+{
+    let add=num1+num2;
+    console.log(add);
+}
+sum(10,20);
