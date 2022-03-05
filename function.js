868d1976f13da2fe17c9f572333ec192bc73a0e7
sabaakram021 <sabaakram211@gmail.com>
2022-03-05 22:42:12 +0530

    function

--- function.js
+++ function.js
@@ -11,3 +11,20 @@ function sum(num1,num2)
     console.log(add);
 }
 sum(10,20);
+
+// function with return type 
+
+function multiply(num1,num2)
+{
+    return num1*num2;
+}
+console.log(multiply(2,3));
+
+
+// functions storing 
+//functions are 1st class citizen
+let a=function sub(num1,num2){
+        return num1-num2;
+    }
+console.log(a(8,2));
+
