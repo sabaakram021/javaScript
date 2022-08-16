a8d28f30e2dc9493fb5687e89d6384f9dbf734cb
sabaakram021 <sabaakram211@gmail.com>
2022-08-16 12:56:38 +0530

    prime check

--- JavaScript/intro.js
+++ JavaScript/intro.js
@@ -2,9 +2,24 @@
 // for(let i=1 ; i<5 ; i++){
 //     console.log(i);
 // }
-let i=1;
+// let i=1;
 
-while(i<5){
-    console.log(i);
-    i++;
+// while(i<5){
+//     console.log(i);
+//     i++;
+// }
+
+let num = 56;
+let flag = 0;
+for(i=2 ;i*i< num ;i++){
+    if(num % i == 0){
+        flag=1;
+        break;
+    }
+}
+if(flag==1){
+    console.log(num + " is not a prime number");
+}
+else{
+    console.log(num + " is a prime number")
 }
