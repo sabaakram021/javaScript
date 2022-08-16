c6e74ec773415c37324f6501e7b32547460c91ce
sabaakram021 <sabaakram211@gmail.com>
2022-08-16 16:52:17 +0530

    array_start

--- JavaScript/intro.js
+++ JavaScript/intro.js
@@ -33,9 +33,18 @@
 
 // ***********************Function with parameter *****************
 
-let a = function sum(num1, num2) {
-    let addtion = num1 + num2;
-    return addtion;
-}
-console.log(a);
-console.log(a(10, 80));
+// let a = function sum(num1, num2) {
+//     let addtion = num1 + num2;
+//     return addtion;
+// }
+// console.log(a);
+// console.log(a(10, 80));
+
+(function(){
+    console.log("from IIFE");
+
+}());
+
+(function(num1,num2){
+    console.log(num1/num2);
+})(10,10);
