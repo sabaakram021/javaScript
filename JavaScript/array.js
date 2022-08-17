96dda64e8518cea7c6b88920c930573c8461ce07
sabaakram021 <sabaakram211@gmail.com>
2022-08-17 12:28:16 +0530

    string methods

--- JavaScript/array.js
+++ JavaScript/array.js
@@ -6,27 +6,29 @@
 /******************* Arrays Method **************** */
 
 let arr=[1,2,3,3,"Hi all",true,3.5,'a'];
-console.log(arr);
+// console.log(arr);
 
-arr[2]='change hua' ;
-console.log(arr);
+// arr[2]='change hua' ;
+// console.log(arr);
 // 1) PUSH ****************
 
-arr.push(99);
-console.log(arr);
+// arr.push(99);
+// console.log(arr);
 
 // 2) POP 
 
-arr.pop();
-console.log(arr);
+// arr.pop();
+// console.log(arr);
 
 // 3) SHIFT
 
-arr.shift();
-console.log(arr);
+// arr.shift();
+// console.log(arr);
 
 
 // 4) UNSHIFT
 
-arr.unshift("Mai aya first me");
-console.log("After unshift",arr);
+// arr.unshift("Mai aya first me");
+// console.log("After unshift",arr);
+
+console.log(arr.length);
--- JavaScript/string.js
+++ JavaScript/string.js
@@ -1,11 +1,28 @@
-let fname="saba";
-let sname=" akram";
-// console.log(fname+" "+sname);
-// let sum=fname.concat(sname);
-// console.log(sum);
-let str="I am a string";
-console.log(str.split(" "));
+let str = "   I am a string";
+console.log(str);
+/********************* Length **********************/
+// console.log(str.length);
 
-let newstring="         saba akram             1";
-console.log(newstring);
-console.log(newstring.trim());
+
+/********************** slice **********************/
+// console.log(str.slice(2,6));
+
+
+/********************** replace ********************/
+// console.log(str.replace("am","was"));
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// let fname = "saba";
+// let lname = " akram";
+// fname[0]='p';
+// console.log(fname);
+// console.log(fname.concat(lname));
+
+
+// let str_arr = str.split(" ");
+// console.log(str_arr);
+
+let trim_str = str.trim();
+console.log(trim_str);
