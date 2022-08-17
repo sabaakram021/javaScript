2fa936e360c65aa245ccc4dcf073150c68dc9f30
sabaakram021 <sabaakram211@gmail.com>
2022-08-17 09:33:06 +0530

    IIFE

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -1,53 +1,21 @@
-// function sayHello()
-// {
-//     console.log("hello from function");
+// ***********************Function with parameter *****************
+
+// let a = function sum(num1, num2) {
+//     let addtion = num1 + num2;
+//     return addtion;
 // }
-// sayHello();
-//function with parameter
+// console.log(a);
+// console.log(a(10, 80));
 
-// function sum(num1,num2)
-// {
-//     let add=num1+num2;
-//     console.log(add);
-// }
-// sum(10,20);
+// (function(){
+//     console.log("from IIFE");
 
-// function with return type 
+// }());
 
-// function multiply(num1,num2)
-// {
-//     return num1*num2;
-// }
-// console.log(multiply(2,3));
+// (function(num1,num2){
+//     console.log(num1/num2);
+// })(10,10);
 
-
-// functions storing 
-//functions are 1st class citizen
-// let a=function sub(num1,num2){
-//         return num1-num2;
-//     }
-// console.log(a(8,2));
-
-//IIFE
-(function(){
-    console.log("IIFI");
-})();
-(function(num1,num2)
-{
-    console.log(num1/num2);
-})(10,2);
-
-
-(function(num1,num2)
-{
-    console.log(num1+num2);
-})(10,93);
-
-
-(function(start,end)
-{
-    for(let i=start;i<=end;i++)
-    {
-        console.log(i);
-    }
-})(1,10);
+(function(num1,num2){
+    console.log(num1 / num2);
+})(7,8);
