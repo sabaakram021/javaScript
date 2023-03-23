7c2315f331d0d966d012f3e8165714c96ae8904d
saba akram <sabaakram211@gmail.com>
2023-03-23 13:12:26 +0530

    object in js

--- object.js
+++ object.js
@@ -2,5 +2,11 @@ let obj1 = {
     'name' : 'Saba Akram',
     'age' : 23,
     phone_number : 9507829261,
-    'country' : "India"
+    address : {
+        city:'ranchi',state:'jharkhand'
+    },
+    sayHI : function(){
+        console.log("Hi, How are you")
+    }
 };
+console.log(obj1);
