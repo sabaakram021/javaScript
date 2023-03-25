ee42f1920f1ddd203ad9d84d8151e9d4aa8e36be
saba akram <sabaakram211@gmail.com>
2023-03-25 11:07:31 +0530

    adding and deleting key

--- object.js
+++ object.js
@@ -9,4 +9,18 @@ let obj1 = {
         console.log("Hi, How are you")
     }
 };
+// console.log(obj1);
+// console.log(obj1.address.city)
+// console.log(obj1.sayHI());
+// obj1.sayHI();
+
+// adding a key 
+// console.log(obj1);
+obj1.id = 2152467;
+// console.log(obj1);
+
+
+//delete a key
+console.log(obj1);
+delete obj1.sayHI;
 console.log(obj1);
